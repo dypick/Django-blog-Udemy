d2ce8b9d04895673062de1fff471b97d17b45159
Xander Abashin <catracoon@yandex.ru>
2019-06-05 23:26:47 +0300

    Tuned template

--- src/posts/views.py
+++ src/posts/views.py
@@ -2,20 +2,47 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Post
 
 def post_create(request):
-	return HttpResponse("<h1>create<h1>")
+	context_data = {
+		"title":"create"
+	}
+	return render(request, "index.html", context_data)
 
 def post_detail(request): #retieve
-	return HttpResponse("<h1>Detail<h1>")
+	context_data = {
+		"title":"Detail"
+	}
+	return render(request, "index.html", context_data)
 
 def post_list(request):
-	return render(request, "index.html", {})
+	queryset = Post.objects.all()
+	# if request.user.is_authenticated():
+	context_data = {
+		"object_list": queryset,
+		"title":" UserList"
+
+	}
+	# else:
+	# 	context_data = {
+	# 	"title":"List"
+
+	# }
+	return render(request, "index.html", context_data)
 
 def post_update(request):
-	return HttpResponse("<h1>Update<h1>")
+	context_data = {
+		"title":"Update"
+
+	}
+	return render(request, "index.html", context_data)
 
 def post_delete(request):
-	return HttpResponse("<h1>Delete<h1>")
+	context_data = {
+		"title":"Delete"
+
+	}
+	return render(request, "index.html", context_data)
 
 
