675b52bfd3d996562355b83dbd3add7018674e8b
Xander Abashin <catracoon@yandex.ru>
2019-06-09 14:43:04 +0300

    Added model form && post_form temp

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,48 +1,46 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .form import PostForm
 from .models import Post
 
 def post_create(request):
-	context_data = {
-		"title":"create"
-	}
-	return render(request, "index.html", context_data)
+	return HttpResponse("<h1>Create</h1>")
+	form = PostForm(request.POST or None)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		print form.cleaned_data.get("title")
+		instance.save()
 
-def post_detail(request): #retieve
-	context_data = {
-		"title":"Detail"
-	}
-	return render(request, "index.html", context_data)
+	
+	context = {
+		"form": form,
+	}	
+	return render(request, "post_form.html", context)
 
-def post_list(request):
+def post_detail(request, id=None): #retrieve
+	#instance = Post.objects.get(id=1)
+	instance = get_object_or_404(Post, id=id)
+	context = {
+		"title": instance.title,
+		"instance": instance,
+	}
+	return render(request, "post_detail.html", context)
+
+
+def post_list(request): #list items
 	queryset = Post.objects.all()
-	# if request.user.is_authenticated():
-	context_data = {
-		"object_list": queryset,
-		"title":" UserList"
-
+	context = {
+		"object_list": queryset, 
+		"title": "List"
 	}
-	# else:
-	# 	context_data = {
-	# 	"title":"List"
+	
+	return render(request, "index.html", context)
 
-	# }
-	return render(request, "index.html", context_data)
 
 def post_update(request):
-	context_data = {
-		"title":"Update"
+	return HttpResponse("<h1>Update</h1>")
 
-	}
-	return render(request, "index.html", context_data)
 
 def post_delete(request):
-	context_data = {
-		"title":"Delete"
-
-	}
-	return render(request, "index.html", context_data)
-
-
+	return HttpResponse("<h1>Delete</h1>")
